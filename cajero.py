ab75fff94ba2b817817558982d259a9808d2b9df
eduardo beleño <luisforero662@gmail.com>
2026-03-06 15:39:42 -0500

    hoy termine el trabajo de cajero automatico

--- cajero.py
+++ cajero.py
@@ -1,28 +1,43 @@
 i=0
 saldo = 1000
 saldo_ini =(f"{saldo:,.2f}")
-num_ope =int(input("ingrese el numero de operaciones:"))
-while (i<num_ope):
+numero_operaciones = int(input("ingrese el numero de operaciones:"))
+
+while i < numero_operaciones :
     print("menu:")
     print("1-> consultar saldo")
     print("2-> retirar dinero")
     print("3-> deporsitar dinero")
-    ope = int(input("elige una opcion:"))
+    print("0-> salir")
+    operacion = int(input("elige una opcion:"))
     
-    if ope == 1 :
+    if operacion == 1 :
         print(f"tu saldo actual es de:{saldo}")
-    elif ope == 2:
-        monto_retirar =float(input("ingrese su monto a retirar:$"))
-        while (monto_retirar > 0) :
-            if monto_retirar > saldo :
-             print("fondos insuficientes")
-            elif monto_retirar >= saldo :
-             print("permitido el retiro")
-            elif monto_retirar < saldo:
-             print(f"su nuevo saldo es de:${saldo - monto_retirar}")
+    elif operacion == 2:
+        while True :
+            monto_retiro = float(input("ingrese su monto a retirar:"))
+            if monto_retiro > saldo :
+                print("fondos insuficientes")
+            elif monto_retiro< 0:
+                print("error,colque un valor correcto.")
+            elif monto_retiro > 0 and monto_retiro <= saldo :
+                print(f"su saldo actual es de:{saldo - monto_retiro}")
+                break
+    elif operacion == 3:
+        while True:
+            monto_consignar = float(input("ingrese su monto a consignar"))
+            if monto_consignar < 0 :
+                print("error, ingrese un valor correcto")
+            elif monto_consignar > 0 :
+                print(f"su saldo actual es de:{saldo + monto_consignar}")
+                break
+    
+    if operacion == 0 :
+        print ("gracias por usar nuestro cajero")
+        break
+    else:
+        print("opcion incorrecta")
+    i+=1        
             
-            break
-        else:
-               print("digite un valor correcto")
- 
-        i+=1     
+
+
